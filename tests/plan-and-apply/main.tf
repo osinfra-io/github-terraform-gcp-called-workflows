1268df4cc59820eda8a0746902c164e723b356b9
Brett Curtis <brett@osinfra.io>
2024-04-23 20:57:43 -0400

    Fix tests

--- tests/plan-and-apply/main.tf
+++ tests/plan-and-apply/main.tf
@@ -20,6 +20,14 @@ module "bucket" {
   source = "github.com/osinfra-io/terraform-google-storage-bucket?ref=v0.1.0"
 
   force_destroy = true
+
+  labels = {
+    cost-center = "x000"
+    env         = var.environment
+    team        = "testing"
+    repository  = "github-terraform-gcp-called-workflows"
+  }
+  
   location      = "US"
   name          = "test-tf5t72-${var.environment}"
   project       = module.test.project_id
